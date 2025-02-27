94b9360dd6ea8d6a525f895413d5dbb03b56f029
Manoel Campos <manoelcampos@gmail.com>
2025-02-27 03:23:18 -0300

    First commit
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

---
+++ src/main/java/io/github/manoelcampos/activerecord/EntityAccessorAgent.java
@@ -0,0 +1,25 @@
+package io.github.manoelcampos.activerecord;
+
+import net.bytebuddy.agent.builder.AgentBuilder;
+import net.bytebuddy.asm.Advice;
+import net.bytebuddy.matcher.ElementMatchers;
+
+import java.lang.instrument.Instrumentation;
+
+import static net.bytebuddy.matcher.ElementMatchers.*;
+
+/**
+ * @author Manoel Campos
+ */
+public class EntityAccessorAgent {
+    public static void premain(String agentArgs, Instrumentation inst) {
+        final var isClassMatcher = not(isRecord().or(isInterface()).or(isAnnotation()));
+        final var entityMatcher = named("jakarta.persistence.Entity");
+
+        new AgentBuilder.Default()
+                .type(isClassMatcher.and(ElementMatchers.isAnnotatedWith(entityMatcher)))
+                .transform((builder, typeDescription, classLoader, module, domain) ->
+                       builder.method(ElementMatchers.isPublic()).intercept(Advice.to(FieldAccessInterceptor.class))
+                ).installOn(inst);
+    }
+}
---
+++ src/main/java/io/github/manoelcampos/activerecord/FieldAccessInterceptor.java
@@ -0,0 +1,44 @@
+package io.github.manoelcampos.activerecord;
+
+import net.bytebuddy.asm.Advice;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author Manoel Campos
+ */
+public class FieldAccessInterceptor {
+    @Advice.OnMethodEnter
+    public static void interceptRead(
+         @Advice.FieldValue(value = "name", readOnly = false) Object fieldValue,
+         final @Advice.This Object instance,
+         final @Advice.Origin("#t.#m") String methodName)
+    {
+        try {
+            final String fieldName = extractFieldName(methodName);
+            final Method getter = instance.getClass().getMethod("get" + capitalize(fieldName));
+            fieldValue = getter.invoke(instance);
+        } catch (Exception ignored) {/**/}
+    }
+
+    @Advice.OnMethodExit
+    public static void interceptWrite(
+          final @Advice.Argument(value = 0) Object newValue,
+          final @Advice.This Object instance,
+          final @Advice.Origin("#t.#m") String methodName)
+    {
+        try {
+            final String fieldName = extractFieldName(methodName);
+            final Method setter = instance.getClass().getMethod("set" + capitalize(fieldName), newValue.getClass());
+            setter.invoke(instance, newValue);
+        } catch (Exception ignored) {/**/}
+    }
+
+    private static String extractFieldName(final String methodName) {
+        return methodName.replace("get", "").replace("set", "");
+    }
+
+    private static String capitalize(final String str) {
+        return str.substring(0, 1).toUpperCase() + str.substring(1);
+    }
+}
