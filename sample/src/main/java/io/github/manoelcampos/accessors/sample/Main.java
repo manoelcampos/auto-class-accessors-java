90a848a1bab87ecdcdab003899315453142d2c2a
Manoel Campos <manoelcampos@gmail.com>
2025-03-17 12:52:17 -0300

    feature: Updates sample
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- sample/src/main/java/io/github/manoelcampos/accessors/sample/Main.java
+++ sample/src/main/java/io/github/manoelcampos/accessors/sample/Main.java
@@ -5,8 +5,8 @@ public class Main {
         final var product = new Product();
         product.id = 1L;
         product.available = true;
-        product.name = "Product Name";
-        product.model = "Product Model";
+        product.name = "TV";
+        product.model = "xy123";
 
         System.out.printf("Product id: %d name: %s model: %s available: %s%n", product.id, product.name, product.model, product.available);
     }
