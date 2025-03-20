9a26ec25a72cd60b116287b88e8dc1f1375c3928
Manoel Campos <manoelcampos@gmail.com>
2025-03-20 13:42:58 -0300

    feature: Updates the Product class
    Adds getter for `available` field and `toString` method.
    
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- sample/src/main/java/io/github/manoelcampos/accessors/sample/Product.java
+++ sample/src/main/java/io/github/manoelcampos/accessors/sample/Product.java
@@ -11,6 +11,11 @@ public class Product {
     public String brand;
     public String model;
     public String name;
+
+    /**
+     * It just uses {@link Boolean} instead of the primitive type {@code boolean} to allow null values
+     * and enable implementing a sample getter with some logic.
+     */
     public Boolean available;
 
     public Product() {
@@ -25,15 +30,22 @@ public class Product {
         this.model = model;
     }
 
-    public void setId(Long id) {
+    public Product setId(Long id) {
         this.id = id >= 0 ? id : 0;
         System.out.printf("Setting product.id. Given value: %s. Stored value: %s%n", id, this.id);
+        return this;
     }
 
     public Long getId() {
         final var id = Objects.requireNonNullElse(this.id, 0L);
         System.out.printf("Getting product.id. Stored value: %s. Returned value: %s%n", this.id, id);
         return id;
+    }
+
+    public boolean isAvailable() {
+        final boolean available = this.available != null && this.available;
+        System.out.printf("Getting product.available. Stored value: '%s'. Returned value: '%s'%n", this.available, available);
+        return available;
     }
 
     public String getModel() {
@@ -50,5 +62,10 @@ public class Product {
     public void setName(final String name) {
         this.name = Objects.requireNonNullElse(name, "").trim();
         System.out.printf("Setting product.name. Given value: '%s'. Stored value: '%s'%n", name, this.name);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{id=%d, brand='%s', model='%s', name='%s', available=%s}".formatted(id, brand, model, name, available);
     }
 }
